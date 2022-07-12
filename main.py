094653452c45ea691b894ff57ad57dc6464f4e57
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-07-12 11:38:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+# importing necessary packages and modules
 from BSmodel import BlackScholes, estimateVolatility
 from twitter import getTweets
 from sentimentAnalysis import generalSentiment
 from options import getOptionInfo
 from sentimentToVol import sentimentToVol
 
+# Initializing the risk free interest rate
 riskFreeIntrestRate = 0.05
 
-# Generates an investment strategy based on the ticker provided
+'''
+Generates an investment strategy based on the ticker provided
+# Get implied volatility given current market conditions
+# Get estimate of option price given by sentiment analysis
+'''
 def generateTradingStrategy(indexTicker:str):
+    
     optionData = getOptionInfo(indexTicker)
     
-    # Get implied volatility given current market conditions
     impliedVol = estimateVolatility(0.0001,
                                     optionData["timetoExp"],
                                     optionData["assetPrice"],
@@ -19,7 +25,7 @@ def generateTradingStrategy(indexTicker:str):
                                     optionData["optionPrice"],
                                     optionData["typeOption"])
     
-    # Get estimate of option price given by sentiment analysis
+    
     optionSentiment = generalSentiment(getTweets("SPX","2022-01-10T13:00:01","2022-01-10T22:00:01",100))
     estimatedVol = sentimentToVol(optionSentiment)
     optionPrice = BlackScholes( optionData["timetoExp"],
@@ -39,4 +45,5 @@ def generateTradingStrategy(indexTicker:str):
     else:
         print(f"Short {optionData['contractSymbol']} until option price is {optionPrice}")
 
+# Calling our generate Trading strategy function on the "SPX" index ticker
 generateTradingStrategy("SPX")
