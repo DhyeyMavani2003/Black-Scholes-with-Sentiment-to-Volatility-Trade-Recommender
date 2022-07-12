9307cfdc52736d244d0b602eb4eee503db62d766
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-07-12 11:45:13 -0400

    Update sentimentToVol.py

--- sentimentToVol.py
+++ sentimentToVol.py
@@ -1,11 +1,12 @@
+# importing keras from TensorFlow
 from tensorflow import keras
 
-# Loading a trained model from Keras from
-#   previous sentiment to volatility data
+# Loading a trained model from Keras from previous sentiment to volatility data
 model = keras.models.load_model('./sentiVol.h5')
 
-# A function to convert sentiment values into a 
-#   predicted volatility score
+'''
+Converts sentiment values into a predicted volatility score
+'''
 def sentimentToVol(sentimentScore):
     ans = model.predict([sentimentScore])
     return ans[0]
